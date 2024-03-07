ea085aba6abea506b9c37b91e25f58a9f607ece9
imanjouhar <136596368+imanjouhar@users.noreply.github.com>
2024-03-07 09:53:52 +0200

    Add files via upload

---
+++ HABIT.sql
@@ -0,0 +1,96 @@
+INSERT INTO HABIT (NAME, START_DATE) VALUES ('Do more exercise', DATE('now'));
+INSERT INTO HABIT (NAME, START_DATE) VALUES ('Eat healthy', DATE('now'));
+INSERT INTO HABIT (NAME, START_DATE) VALUES ('Become more knowledgeable', DATE('now'));
+INSERT INTO HABIT (NAME, START_DATE) VALUES ('Become more organized', DATE('now'));
+INSERT INTO HABIT (NAME, START_DATE) VALUES ('Live Healthier', DATE('now'));
+INSERT INTO HABIT (NAME, START_DATE) VALUES ('Do more volunteering', DATE('now'));
+
+INSERT INTO TASK_HISTORY (TASK_ID, EVENT_DATE, STATUS_CHANGE) VALUES
+(40, '2023-12-26', 'Checked'),
+(40, '2023-12-27', 'Checked'),
+(40, '2023-12-28', 'Checked'),
+(40, '2023-12-29', 'Checked'),
+(40, '2023-12-30', 'Checked'),	
+(40, '2023-12-31', 'Checked'),
+(40, '2024-01-01', 'Checked'),
+(40, '2024-01-02', 'Checked'),
+(40, '2024-01-03', 'Checked'),
+(40, '2024-01-04', 'Checked'),
+(40, '2024-01-05', 'Checked'),
+(40, '2024-01-06', 'Checked'),
+(40, '2024-01-07', 'Checked'),
+(40, '2024-01-08', 'Checked'),
+(40, '2024-01-09', 'Checked'),
+(40, '2024-01-10', 'Checked'),
+(40, '2024-01-11', 'Checked'),
+(40, '2024-01-12', 'Checked'),
+(40, '2024-01-13', 'Checked'),
+(40, '2024-01-14', 'Checked'),
+(40, '2024-01-15', 'Checked'),
+(40, '2024-01-16', 'Checked'),
+(40, '2024-01-17', 'Checked'),
+(40, '2024-01-18', 'Checked'),
+(40, '2024-01-19', 'Checked'),
+(40, '2024-01-20', 'Checked'),
+(40, '2024-01-21', 'Checked'),
+(40, '2024-01-22', 'Checked');
+
+INSERT INTO TASK_HISTORY (TASK_ID, EVENT_DATE, STATUS_CHANGE) VALUES
+(41, '2023-12-26', 'Checked'),
+(41, '2023-12-27', 'Checked'),
+(41, '2023-12-28', 'Checked'),
+(41, '2023-12-29', 'Checked'),
+(41, '2023-12-30', 'Checked'),
+(41, '2023-12-31', 'Checked'),
+(41, '2024-01-01', 'Checked'),
+(41, '2024-01-02', 'Checked'),
+(41, '2024-01-03', 'Checked'),
+(41, '2024-01-04', 'Checked'),
+(41, '2024-01-05', 'Checked'),
+(41, '2024-01-06', 'Checked'),
+(41, '2024-01-07', 'Checked'),
+(41, '2024-01-08', 'Checked'),
+(41, '2024-01-09', 'Checked'),
+(41, '2024-01-10', 'Checked'),
+(41, '2024-01-11', 'Checked'),
+(41, '2024-01-12', 'Checked'),
+(41, '2024-01-13', 'Checked'),
+(41, '2024-01-14', 'Checked'),
+(41, '2024-01-15', 'Checked'),
+(41, '2024-01-16', 'Checked'),
+(41, '2024-01-17', 'Checked'),
+(41, '2024-01-18', 'Checked'),
+(41, '2024-01-19', 'Checked'),
+(41, '2024-01-20', 'Checked'),
+(41, '2024-01-21', 'Checked'),
+(41, '2024-01-22', 'Checked');
+
+-- Task ID 25: Go for a run
+INSERT INTO TASK_HISTORY (TASK_ID, EVENT_DATE, STATUS_CHANGE) VALUES
+(25, '2023-12-25', 'Checked'),
+(25, '2024-01-01', 'Checked'),
+(25, '2024-01-08', 'Checked'),
+(25, '2024-01-15', 'Checked');
+
+-- Task ID 27: Help the charity shop
+INSERT INTO TASK_HISTORY (TASK_ID, EVENT_DATE, STATUS_CHANGE) VALUES
+(27, '2023-12-25', 'Checked'),
+(27, '2024-01-01', 'Checked'),
+(27, '2024-01-08', 'Checked'),
+(27, '2024-01-15', 'Checked');
+
+-- Task ID 22: Pay utility bills
+INSERT INTO TASK_HISTORY (TASK_ID, EVENT_DATE, STATUS_CHANGE) VALUES
+(22, '2023-12-22', 'Checked');
+
+-- Task ID 26: Help the ambulance service
+INSERT INTO TASK_HISTORY (TASK_ID, EVENT_DATE, STATUS_CHANGE) VALUES
+(26, '2023-12-22', 'Checked');
+
+-- Task ID 32: Politely eat dinner
+INSERT INTO TASK_HISTORY (TASK_ID, EVENT_DATE, STATUS_CHANGE) VALUES
+(32, '2023-12-23', 'Checked');
+
+-- Task ID 33: Do Push ups
+INSERT INTO TASK_HISTORY (TASK_ID, EVENT_DATE, STATUS_CHANGE) VALUES
+(33, '2023-12-23', 'Checked');
